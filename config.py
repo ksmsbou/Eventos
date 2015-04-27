92254d21f5196bb3afefd40415a70fbe9d95bf34
Jhon Villamizar <alvaro92092@gmail.com>
2015-04-26 23:54:30 -0430

    Primera Version de la BD
    Primera version de la Base de Datos

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ OPENID_PROVIDERS = [
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'apl.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
